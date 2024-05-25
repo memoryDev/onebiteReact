cd59e302fcad1291247cf8d916584249896da33e
memoryDev <memorydev2024@gmail.com>
2024-05-25 09:57:16 +0900

    5.10)useRef로 컴포넌트의 변수 생성하기

--- section05/src/component/Register.jsx
+++ section05/src/component/Register.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 
 // 간단한 회원가입 폼
 // 1.이름
@@ -7,38 +7,86 @@ import { useState } from "react";
 // 4.자기소개
 
 const Register = () => {
-  const [name, setName] = useState("이름");
-  const [birth, setBirth] = useState("");
-  const [contry, setContry] = useState("");
-  const [bio, setBio] = useState("");
+  //   const [name, setName] = useState("이름");
+  //   const [birth, setBirth] = useState("");
+  //   const [contry, setContry] = useState("");
+  //   const [bio, setBio] = useState("");
+  const [input, setInput] = useState({
+    name: "",
+    birth: "",
+    contry: "",
+    bio: "",
+  });
 
-  const onChangeName = (e) => {
-    setName(e.target.value);
+  const countRef = useRef(0);
+  const inputRef = useRef();
+
+  const onChange = (e) => {
+    countRef.current++;
+    console.log(countRef.current);
+    setInput({
+      ...input,
+      [e.target.name]: e.target.value,
+    });
   };
 
-  const onChangeBirth = (e) => {
-    setBirth(e.target.value);
-  };
+  //   const onChangeName = (e) => {
+  //     setInput({
+  //       ...input,
+  //       name: e.target.value,
+  //     });
+  //   };
 
-  const onChangeContry = (e) => {
-    setContry(e.target.value);
-  };
+  //   const onChangeBirth = (e) => {
+  //     setInput({
+  //       ...input,
+  //       birth: e.target.value,
+  //     });
+  //   };
 
-  const onChangeBio = (e) => {
-    setBio(e.target.value);
+  //   const onChangeContry = (e) => {
+  //     setInput({
+  //       ...input,
+  //       contry: e.target.value,
+  //     });
+  //   };
+
+  //   const onChangeBio = (e) => {
+  //     setInput({
+  //       ...input,
+  //       bio: e.target.value,
+  //     });
+  //   };
+
+  const onSubmit = () => {
+    if (input.name === "") {
+      //이름을 입력하는 DOM 요소 포커스
+      inputRef.current.focus();
+    }
   };
 
   return (
     <>
       <div>
         <div>
-          <input value={name} onChange={onChangeName} placeholder="이름" />
+          <input
+            ref={inputRef}
+            name="name"
+            value={input.name}
+            onChange={onChange}
+            placeholder="이름"
+          />
         </div>
         <div>
-          <input value={birth} onChange={onChangeBirth} type="date" />
+          <input
+            name="birth"
+            value={input.birth}
+            onChange={onChange}
+            type="date"
+          />
         </div>
         <div>
-          <select value={contry} onChange={onChangeContry}>
+          <select name="contry" value={input.contry} onChange={onChange}>
             <option value=""></option>
             <option value="kr">한국</option>
             <option value="us">미국</option>
@@ -46,9 +94,10 @@ const Register = () => {
           </select>
         </div>
         <div>
-          <textarea onChange={onChangeBio} value={bio} />
-          {bio}
+          <textarea name="bio" onChange={onChange} value={input.bio} />
         </div>
+
+        <button onClick={onSubmit}>제출</button>
       </div>
     </>
   );
