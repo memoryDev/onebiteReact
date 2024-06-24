66b3781688e2b21c0819cd834ac43d8df6e8efc1
memoryDev <115910144+memoryDev@users.noreply.github.com>
2024-06-24 14:26:08 +0900

    연산자

---
+++ section01/chapter07.js
@@ -0,0 +1,23 @@
+// 1. 대입 연산자
+let var1 = 1;
+
+// 2. 산술 연산자
+let num1 = 3 + 2;
+let num2 = 3 - 2;
+let num3 = 3 * 2;
+let num4 = 3 / 2;
+let num5 = 3 % 2;
+
+let num6 = 1 + 2 * 10; //21(연산자 우선순위)
+// console.log(num6);
+num6 = (1 + 2) * 10;
+// console.log(num6);
+
+// 3. 복합 대입 연산자
+let num7 = 10;
+// num7 = num7 + 20;
+num7 += 20;
+num7 -= 20;
+num7 *= 20;
+num7 /= 20;
+num7 %= 10;
