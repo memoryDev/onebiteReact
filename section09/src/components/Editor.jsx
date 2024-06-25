6402bce0f3e83c809794f3362f41dd806e9be131
memoryDev <115910144+memoryDev@users.noreply.github.com>
2024-06-25 17:25:07 +0900

    투두리스트 업데이트/useReducer 실습

---
+++ section09/src/components/Editor.jsx
@@ -0,0 +1,43 @@
+import "./Editor.css";
+
+import { useState, useRef } from "react";
+
+const Editor = ({ onCreate }) => {
+  const [content, setContent] = useState("");
+  const contentRef = useRef();
+
+  const onSubmit = function () {
+    if (content === "") {
+      contentRef.current.focus();
+      return;
+    }
+    onCreate(content);
+    setContent("");
+  };
+
+  const onKeydown = (e) => {
+    if (e.keyCode === 13) {
+      onSubmit();
+    }
+  };
+
+  const onChangeContent = (e) => {
+    setContent(e.target.value);
+  };
+
+  return (
+    <div className="Editor">
+      <input
+        ref={contentRef}
+        value={content}
+        type="text"
+        placeholder="새로운 Todo..."
+        onChange={onChangeContent}
+        onKeyDown={onKeydown}
+      />
+      <button onClick={onSubmit}>추가</button>
+    </div>
+  );
+};
+
+export default Editor;
